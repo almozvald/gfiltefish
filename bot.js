4d339a6d8c75fb46e6f5b210c2a2940fd458d051
Electric <itai.levin4@gmail.com>
2020-07-14 00:33:59 +0300

    1.1.2

--- bot.js
+++ bot.js
@@ -8,6 +8,9 @@ const client = new Discord.Client();
 client.login(auth.token);
 'use strict';
 
+// Channels
+var ch_general, ch_polin, ch_bulgaria, ch_natania, ch_georgia, ch_gointg, ch_meandbois;
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -29,6 +32,15 @@ client.on("ready", () => {
 	logger.info("Connected");
 	logger.info("Logged in as: ");
 	logger.info(client.user.username + " - (" + client.user.id + ")");
+
+	// Setting channels IDs
+	ch_general   = client.channels.cache.get('369817614890958852');
+	ch_polin     = client.channels.cache.get('410127563650760704');
+	ch_bulgaria  = client.channels.cache.get('417774179237101580');
+	ch_natania   = client.channels.cache.get('466007520918634507');
+	ch_georgia   = client.channels.cache.get('731917741891649627');
+	ch_gointg    = client.channels.cache.get('369951844694228994');
+	ch_meandbois = client.channels.cache.get('410128501358854174');
 });
 
 var curchannel;
@@ -52,42 +64,61 @@ var intervaled = function(annoychannel, annoymessage, timesleft , interval) {
 	}
 }
 
-client.on("message", msg => { // (user, userID, channelID, message, evt) 
-	var message = msg.content;
-	var user = msg.author;
-	var userID = user.id;
-	var channel = msg.channel;
+var channel_warp = function(ch_current, plus_mode) {
+	var others = msg.guild.channels.cache.filter(c => (c.ch_current === ch_current.parentID || plus_mode) && c.type === 'voice').array();
+	for (var i = 0; i < others.length; i++) {
+		var otherchannel = others[i];
+		if (otherchannel.id == ch_current.id)
+			continue;
+
+		otherchannel.members.each(user => user.voice.setChannel(ch_current));
+	}
+}
+
+client.on("message", msg => {
+	var message   = msg.content;
+	var user      = msg.author;
+	var userID    = user.id;
+	var channel   = msg.channel;
 	var channelID = msg.channel.id;
-	if (message.indexOf("פולין") != -1 || message.indexOf("polin") != -1)
+
+    if (user.bot) {
+        return;
+        logger.info("what?");
+    }
+
+	if (message.indexOf("פולין") != -1 || message.indexOf("polin") != -1 || message.indexOf("poland") != -1 || message.indexOf("polski") != -1)
 			msg.react("🇵🇱");
+
+    if (message.indexOf("ישראל") != -1 || message.indexOf("israel") != -1)
+            msg.react("🇮🇱");
+
 	for (var i = 0; i < people.length; i++) {
-			var found = false;
-			for (var j = 0; j < people[i].names.length; j++) {
-				if (message.indexOf(people[i].names[j]) != -1) {
-					found = true;
-					break;
-				}
-			}
-			if (msg.guild && msg.guild.emojis && found) {
-				const emoji = msg.guild.emojis.cache.get(people[i].emoji);
-				if (emoji)
-					msg.react(emoji);
+		var found = false;
+		for (var j = 0; j < people[i].names.length; j++) {
+			if (message.indexOf(people[i].names[j]) != -1) {
+				found = true;
+				break;
 			}
 		}
+		if (msg.guild && msg.guild.emojis && found) {
+			const emoji = msg.guild.emojis.cache.get(people[i].emoji);
+			if (emoji)
+				msg.react(emoji);
+		}
+	}
+
 	if (message.substring(0, prefix.length) == prefix) { // A direct call for gfiltefish
 		curchannel = channel;
 		var args = message.substring(prefix.length).split(" ");
 		var cmd = args[0];
 		args = args.splice(1);
 		switch (cmd) {
-			case "help":
-			case "h":
-			case "iamdumb":
-			case "carmi":
+			case "help": case "h": case "iamdumb": case "carmi":
 				var message="";
 
 				var documntation = [
-					"ברוך הבא לגפילטאפיש גרסא 1.1.1",
+					"ברוך הבא לגפילטאפיש גרסא 1.1.2",
 					"מצורפת רשימה של כל הפקודות החוקיות:",
 					"~help " + " קבל את ההודעה הזאת",
 					"~ping " + " בדוק האם הבוט הזה חי",
@@ -103,91 +134,84 @@ client.on("message", msg => { // (user, userID, channelID, message, evt)
 				];
 
 				documntation[documntation.length - 1] = shutup ? '~repeat' + 'הבוט כרגע מושתק אז שום הודעה לא תקרה עם תשתמש ב' : 'בוט כרגע לא מושתק אז' + ' repeat~ ' + 'עובד לכולם';
-			
+
 				for (var i = 0; i < documntation.length; i++)
 					message += documntation[i] + '\n';
-				
+
 				channel.send(message);
 				break;
+
 			case "ping":
 				channel.send("פונג");
 				break;
+
 			case "hello":
 				channel.send("אני חי כמו תמיד");
 				break;
-			case "mute":
-			case "shutup":
+
+			case "mute": case "shutup":
 				shutup = true;
 				channel.send("<@" + userID + "> טוב אני אשב לי לבד בחושך");
 				break;
-			case "unmute":
-			case "unshutup":
+
+			case "unmute": case "unshutup":
 				shutup = false;
 				channel.send("<@" + userID + "> תודה לך צדיק");
 				break;
-			case "polin":
-			case "poland":
-			case "פולין":
-				var poland = client.channels.cache.get('410127563650760704');
-				var others = msg.guild.channels.cache.filter(c => c.parentID === poland.parentID && c.type === 'voice').array();
-				for (var i = 0; i < others.length; i++) {
-					var otherchannel = others[i];
-					if(otherchannel.id === poland.id){
-						continue;
-					}
-					otherchannel.members.each(user => user.voice.setChannel(poland));
-				}
+
+			case "polin": case "poland": case "פולין": case "polski":
+				channel_warp(ch_polin, false);
 				break;
-			case "polin+":
-			case "poland+":
-			case "פולין+":
-				var poland = client.channels.cache.get('410127563650760704');
-				var others = msg.guild.channels.cache.filter(c => c.type === 'voice').array();
-				for (var i = 0; i < others.length; i++) {
-					var otherchannel = others[i];
-					if(otherchannel.id == poland.id){
-						continue;
-					}
-					otherchannel.members.each(user => user.voice.setChannel(poland));
-				}
+
+			case "polin+": case "poland+": case "פולין+": case "polski+":
+				channel_warp(ch_polin, true);
 				break;
-			case "load":
-			case "reload":
+
+			case "Bulgaria":: case "בולגריה":
+				channel_warp(ch_bulgaria, false);
+				break;
+
+			case "Bulgaria+": case "בולגריה+":
+				channel_warp(ch_bulgaria, true);
+				break;
+
+			case "load": case "reload":
 				var fs = require("fs");
 				fs.readFile('./data.json', function (err, newdata) {
 					if (err)
-						throw err; 
-					
+						throw err;
+
 					logger.info(data);
 					data = JSON.parse(newdata);
 					people = data.people;
 					quotes = data.quotes;
 					channel.send("נטען בהצלחה!");
 				});
-				
+
 				channel.send("טוען מחדש");
 				break;
-			case "hhbetza":
-				channel.send("https://i.imgur.com/AVDMBal.png");
+
+			case "hhbetza": case "חח בצה": case "חחבצה":
+				channel.send("", {files: ["https://i.imgur.com/AVDMBal.png"]});
 				break;
-			case "sourcecode":
-			case "github":
-			case "source":
-			case "src":
+
+			case "sourcecode": case "github": case "source": case "src":
 				channel.send('https://github.com/almozvald/gfiltefish');
 				break;
+
 			case "rickroll":
-				var possibleresponses = ["קבלו איפון 23 חינם','צפו בתמונות עירום של בניו",
-					"זהו אינו קישור לשיר אינני הולך לותר עליך על ידי ריק אסטלי",
+				var possible_responses = ["קבלו איפון 23 חינם','צפו בתמונות עירום של בניו",
+					"זהו אינו קישור לשיר אינני הולך לוותר עליך על ידי ריק אסטלי",
 					"קבלו את התפקיד חציל בתריק הפשוט הבא"];
-				
+
 				const embed = new Discord.MessageEmbed()
-					.setTitle(possibleresponses[Math.floor(Math.random() * possibleresponses.length)])
+					.setTitle(possible_responses[Math.floor(Math.random() * possible_responses.length)])
 					.setColor(0x0000ff)
 					.setURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-					
+
 				channel.send(embed);
 				break;
+
 			case "repeat":
 				logger.info(channel);
 				var message = "";
@@ -228,49 +252,42 @@ client.on("message", msg => { // (user, userID, channelID, message, evt)
 				}
 				intervaled(channel, message, timesleft, interval);
 				break;
-			case "randomquote":
-			case "rndq":
+
+			case "randomquote": case "rndq":
 				randomquote(channelID);
 				break;
-			case "allquote":
-			case "allq":
+
+			case "allquote": case "allq":
 				allquote(channelID);
 				break;
+
 			default :
 				channel.send(":לא ממש הבנתי מה אתה מנסה להגיד הפקודות החוקיות הן" + "\n~help");
 		}
 	} else {
-		if (user.bot) {
-			return;
-			logger.info("what?");
-		}
-		
+
 		if (message.indexOf(client.user.id) != -1 || message.indexOf(client.user.username) != -1) {
 			logger.info("bot.id: " + message.indexOf(client.user.id));
 			logger.info("bot.username: " + message.indexOf(client.user.username));
-			var possibleresponses = ["מי זה מדבר עלי?" ,"מה אתה רוצה ממני?" ,"הי מה אתה חושב שאתה אומר עלי?"];
-			channel.send(possibleresponses[Math.floor(Math.random() * possibleresponses.length)] + "\n נסה ~help לעזרה בשביל להבין עלי יותר");
+			var possible_responses = ["מי זה מדבר עלי?" ,"מה אתה רוצה ממני?" ,"הי מה אתה חושב שאתה אומר עלי?"];
+			channel.send(possible_responses[Math.floor(Math.random() * possible_responses.length)] + "\n נסה ~help לעזרה בשביל להבין עלי יותר");
 			return;
 		}
-		
+
 		if (message.indexOf("כרמי") != -1 || message.indexOf("282820918298804224") != -1) {
-			var possibleresponses=["1+e^i(pi)", "0", "אפס"];
-			channel.send(possibleresponses[Math.floor(Math.random() * possibleresponses.length)]);
+			var possible_responses = ["1+e^i(pi)", "0", "אפס"];
+			channel.send(possible_responses[Math.floor(Math.random() * possible_responses.length)]);
 		}
-		
-		
-		
-		
-		
+
 		if (message.indexOf("חח בצה") != -1)
 			channel.send("", {files: ["https://i.imgur.com/AVDMBal.png"]});
-		
+
 		if (message.indexOf("אמור") != -1)
 			channel.send("אמור זה שם של דג");
-		
-		if (Math.random() < 0.01) {
-			var possibleresponses=['שתוק'];
-			channel.send("<@" + userID + "> " + possibleresponses[Math.floor(Math.random() * possibleresponses.length)]);
+
+		if (Math.random() < ((userID == "282820918298804224") ? 0.05 : 0.01)) {
+			var possible_responses = ['שתוק'];
+			channel.send("<@" + userID + "> " + possible_responses[Math.floor(Math.random() * possible_responses.length)]);
 			logger.info("answering random message");
 		}
 
